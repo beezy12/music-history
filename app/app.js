4507f1ab899d3b8f090242fb78cb865ab8598a90
Brian Stumbaugh <brian.stumbuagh@gmail.com>
2015-12-03 18:44:00 -0600

    got first partial populating songs

--- app/app.js
+++ app/app.js
@@ -1,17 +1,21 @@
-var app = angular.module("MusicApp", ["ngRoute"]);
-
-app.config(['$routeProvider',
-  function($routeProvider) {
+angular
+	.module("MusicApp", ["ngRoute"])
+	.config(['$routeProvider',
+  	function($routeProvider) {
     $routeProvider
-    .when('/songs/list', {
-        templateUrl: 'partials/songList.html',
-        controller: 'MusicCtrl'
-      })
-      .when('/songs/form', {
-        templateUrl: 'partials/songForm.html',
-        controller: 'MusicCtrl'
-      })
-      .otherwise('/songs/list');
+    	.when('/songs/main', {
+    		templateUrl: 'partials/songOutput.html',
+    		controller: 'MusicCtrl'
+    	})
+    	.when('/songs/list', {
+        	templateUrl: 'partials/songList.html',
+        	controller: 'MusicCtrl'
+		})
+		.when('/songs/form', {
+			templateUrl: 'partials/songForm.html',
+			controller: 'MusicCtrl'
+		})
+		.otherwise('/songs/list');
   }]);
 
 
--- controllers/songController.js
+++ controllers/songController.js
@@ -1,13 +1,15 @@
-var app = angular.module("MusicApp");
-
-app.controller("MusicCtrl", ["$q", "$http", "$scope", "simple-songs", function($q, $http, $scope, simpleSongs) {
+angular
+	.module("MusicApp")
+	.controller("MusicCtrl", ["$q", "$http", "$scope", "simpleSongs", function($q, $http, $scope, simpleSongs) {
 
 	$scope.searchText = "";
 	$scope.songs = [];
 	
 	simpleSongs.loadSongs().then(
 		function () {
+			
 			$scope.songs = simpleSongs.getSongs();
+			console.log($scope.songs);
 		},
 		function (error) {
 			console.log("error", error);
